5a9e4521536f11966dd483360d02953d7a2a4fff
superpro1226 <javonwoods252@gmail.com>
2024-04-22 16:31:39 -0700

    updated education

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -34,7 +34,7 @@ function Education() {
       </div>
 
       <div className="py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
           <div className="flex justify-center items-start">
             <div className="w-3/4 h-3/4">
               <AnimationLottie animationPath={lottieFile} />
@@ -42,11 +42,11 @@ function Education() {
           </div>
 
           <div>
-            <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-6 ">
               {
                 educations.map(education => (
                   <GlowCard key={education.id} identifier={`education-${education.id}`}>
-                    <div className="p-3 relative text-white">
+                    <div className="p-6 relative text-white ">
                       <Image
                         src="/blur-23.svg"
                         alt="Hero"
